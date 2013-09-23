2edb5a4414d99e3e0b76adb8f47760f44ff97b76
petejank <piotr.jankowski87@gmail.com>
2013-09-23 02:16:11 +0200

    added some changes in formatting

--- scripts/directives/headerMenu.js
+++ scripts/directives/headerMenu.js
@@ -1,15 +1,18 @@
 'use strict';
 
 /**
- * A simple directive used to adjust the class of header menu anchors based on their classes
+ * A simple directive used to adjust the class of header 
+ * menu anchors based on their classes
  */
 angular.module('AngularApp1')
   .directive('headerMenu', function () {
     
     return function(scope, element) {
-      // Watch "activePage" variable created in MainCtrl controller's scope
+      // Watch "activePage" variable created in MainCtrl 
+      // controller's scope
       scope.$watch('activePage', function(value) {
-        // On "activePage" variable change in scope - adjusted currently active page link in header
+        // On "activePage" variable change in scope - adjusted 
+        // currently active page link in header
         var activeLink = element.find('.' + value),
             activeClass = 'active';
         // Strip all anchors elements from "active" class
